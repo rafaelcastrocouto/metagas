4e8ef8786ab0f8de2c32429d2d937a913de58718
@racascou <rafaelcastrocouto@gmail.com>
2025-11-13 10:29:26 -0300

    Add PHP server logo to footer
    Added a PHP server logo image and updated the footer to include a link to php.net with the new logo.

--- templates/element/footer.php
+++ templates/element/footer.php
@@ -1,5 +1,6 @@
 <!-- templates/element/footer.php -->
 <footer>
+  <a href="https://www.php.net/" target="_blank" rel="noopener"><img alt="PHPserver" src="<?= $this->Html->Url->build('/') ?>/img/php-server.png"></a>
   <a href="https://github.com/rafaelcastrocouto/metagas" target="_blank" rel="noopener"><img alt="Git" src="<?= $this->Html->Url->build('/') ?>/img/git-logo.png"></a>
   <a href="https://cakephp.org/" target="_blank" rel="noopener"><img alt="CakePHP" src="<?= $this->Html->Url->build('/') ?>/img/cake-logo.png"></a>
 </footer>
